39ea92c2da5f7ccde7cf081a67a6cd412f01b8ce
thisu1930469 <thisu1930469@etucegepjonquiere.ca>
2021-04-08 13:59:18 -0400

    Ajoutez des fichiers projet.

---
+++ Data/MvcVetementContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MvcVetement.Models;
+
+namespace MvcVetement.Data
+{
+    public class MvcVetementContext : IdentityDbContext
+    {
+        public MvcVetementContext(DbContextOptions<MvcVetementContext> options)
+                : base(options)
+        {
+        }
+        public DbSet<Vetement> Vetement { get; set; }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcVetement.Data;
+using System;
+using System.Linq;
+
+namespace MvcVetement.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcVetementContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcVetementContext>>()))
+            {
+                // Look for any movies.
+                if (context.Vetement.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Vetement.AddRange(
+                    new Vetement
+                    {
+                        Title = "Marinière",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Homme",
+                        Rating = "R",
+                        Price = 24.99M
+                    },
+
+                    new Vetement
+                    {
+                        Title = "Jeans ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Mixte",
+                        Rating = "R",
+                        Price = 40M
+                    },
+
+                    new Vetement
+                    {
+                        Title = "Robe 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Femme",
+                        Rating = "R",
+                        Price = 200M
+                    },
+
+                    new Vetement
+                    {
+                        Title = "Foulard",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Mixte",
+                        Rating = "R",
+                        Price = 10M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
